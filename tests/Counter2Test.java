af82d1f20f362d0623d5e6ce821bb87c138a936c
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-02-19 16:02:11 +0000

    Written initial test.

--- tests/Counter2Test.java
+++ tests/Counter2Test.java
@@ -1,6 +1,7 @@
 package tests;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 import org.junit.Test;
 
@@ -11,7 +12,32 @@ public class Counter2Test {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+
+		Integer first,mockedFirst;
+		Integer second,mockedSecond;
+		
+		Counter2 count = new Counter2();
+		
+		Counter2 mockedCounter = mock(Counter2.class);
+		when(mockedCounter.getValue(1)).thenReturn(1).thenReturn(2);
+		when(mockedCounter.getValue(2)).thenReturn(2).thenReturn(4);
+		
+		first = count.getValue(1);
+		second = count.getValue(2);
+		mockedFirst = mockedCounter.getValue(1);
+		mockedSecond = mockedCounter.getValue(2);
+		
+		assertEquals("First first wronge Answer !",first , mockedFirst);
+		assertEquals("First second Wronge Answer !",second , mockedSecond);
+		
+		first = count.getValue(1);
+		second = count.getValue(2);
+		mockedFirst = mockedCounter.getValue(1);
+		mockedSecond = mockedCounter.getValue(2);
+		
+		assertEquals("Second first Answer !",first , mockedFirst);
+		assertEquals("Second second Answer !",second , mockedSecond);
+		
 	}
 
 }
