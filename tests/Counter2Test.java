fe709464d31a3c330ec2263b87c9ce6883a417a5
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-02-19 16:35:45 +0000

    Added 3.

--- tests/Counter2Test.java
+++ tests/Counter2Test.java
@@ -15,28 +15,33 @@ public class Counter2Test {
 
 		Integer first, mockedFirst;
 		Integer second, mockedSecond;
+		Integer third, mockedThird;
 		
-		Counter2 count = new Counter2();
+		Counter2 counter = new Counter2();
 		
 		Counter2 mockedCounter = mock(Counter2.class);
-		when(mockedCounter.getValue(1)).thenReturn(1).thenReturn(2);
-		when(mockedCounter.getValue(2)).thenReturn(2).thenReturn(4);
+		when(mockedCounter.getValue(1)).thenReturn(1).thenReturn(2);//This tells Mockito what to answer on invocation with ‘1’
+		when(mockedCounter.getValue(2)).thenReturn(2).thenReturn(4);//This tells Mockito what to answer on invocation with ‘2’
+		when(mockedCounter.getValue(3)).thenReturn(-1);
 		
-		first = count.getValue(1);
-		second = count.getValue(2);
+		first = counter.getValue(1);
+		second = counter.getValue(2);
+		third = counter.getValue(3);
 		mockedFirst = mockedCounter.getValue(1);
 		mockedSecond = mockedCounter.getValue(2);
+		mockedThird = mockedCounter.getValue(3);
 		
 		assertEquals("First first wrong Answer !", first, mockedFirst);
 		assertEquals("First second Wrong Answer !", second, mockedSecond);
+		assertEquals("First third wrong answer!", third, mockedThird);
 		
-		first = count.getValue(1);
-		second = count.getValue(2);
+		/*first = counter.getValue(1);
+		second = counter.getValue(2);
 		mockedFirst = mockedCounter.getValue(1);
 		mockedSecond = mockedCounter.getValue(2);
 		
 		assertEquals("Second first Answer !", first, mockedFirst);
-		assertEquals("Second second Answer !", second, mockedSecond);
+		assertEquals("Second second Answer !", second, mockedSecond);*/
 		
 	}
 
