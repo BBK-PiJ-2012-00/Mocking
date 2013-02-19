b1be7d0eae24e5fac6b1229b4564dcea8b75c484
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-02-19 15:46:45 +0000

    Update.

---
+++ tests/Counter2Test.java
@@ -0,0 +1,17 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import code.Counter2;
+
+
+public class Counter2Test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
