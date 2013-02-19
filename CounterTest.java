f43eeec8864ea5df61b3aa8f1c2b7cb6fc43b151
BBK-PiJ-2012-00 <ataylo20@mail.bbk.ac.uk>
2013-02-19 14:59:01 +0000

    Added test for verify atMost().

--- CounterTest.java
+++ CounterTest.java
@@ -39,6 +39,34 @@ public class CounterTest {
 		
 		verify(mockedCounter,atLeast(4)).getValue(); //This tells Mockito to check that ‘getValue’ has been called at least 4 times
 	}
-
+	
+	@Test
+	public void test2() {
+		
+		Integer first, mockedFirst;
+		Integer expectedFirst = 1;
+		Counter count = new Counter();
+		
+		Counter mockedCounter = mock(Counter.class); //This tells Mockito to create a class of type mock
+		when(mockedCounter.getValue()).thenReturn(1).thenReturn(2); //This tells Mockito that when getValue() is called the first 
+		//time it should return 1, then 2 etc
+		
+		first = count.getValue();
+		mockedFirst = mockedCounter.getValue(); //Mocked class interfaced like the real thing
+		
+		assertEquals("Wrong Answer!", expectedFirst, first);
+		
+		first = count.getValue();
+		mockedFirst = mockedCounter.getValue();
+		
+		assertEquals("Wrong Answer !", first, mockedFirst);		
+		
+		verify(mockedCounter,atMost(2)).getValue(); //This tells Mockito to check that ‘getValue’ has been called at least 4 times
+	}
+		
 }
+	
+	
+
+
 
